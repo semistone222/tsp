3706bc40c0c8ac99a7dd4e15f583fdbcf3511ccb
semistone222 <semistone222@gmail.com>
2017-09-25 03:36:24 +0900

    updated TODOs

--- src/greedy/TwoOptSearch.java
+++ src/greedy/TwoOptSearch.java
@@ -23,6 +23,13 @@ public class TwoOptSearch extends TSP {
         int trial = 0;
         while(trial < limitTrial) {
             // TODO : Deep Copy
+            // https://geunhokim.wordpress.com/2013/06/15/deep-copy-shallow-copy/
+            // TODO : 1) Random Pick
+            // TODO : 2) 2 Long Edge Pick With Tabu Memory
+            // http://asuraiv.blogspot.kr/2015/11/java-priorityqueue.html
+            // TODO : 3) Weighted Random Pick
+            // https://www.google.co.kr/search?q=java+weighted+random&oq=java+weighted+random&aqs=chrome..69i57j69i60l2j0l3.4539j0j7&sourceid=chrome&ie=UTF-8
+
         }
         return null;
     }
--- src/sa/SASearch.java
+++ src/sa/SASearch.java
@@ -1,4 +1,6 @@
 package sa;
 
 public class SASearch {
+    // TODO : cooling function
+    // http://what-when-how.com/artificial-intelligence/a-comparison-of-cooling-schedules-for-simulated-annealing-artificial-intelligence/
 }
