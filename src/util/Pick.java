2443627db91a4ea0cb88c82f718957239becb073
noname2048 <noname2048@users.noreply.github.com>
2017-09-26 12:44:56 +0900

    threeOpt added

--- src/util/Pick.java
+++ src/util/Pick.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.ArrayList;
+
 public class Pick {
     public static int[] getTwoRandomIndex(int lower, int higher) {
         int first = getRandomInt(lower, higher);
@@ -19,5 +21,23 @@ public class Pick {
     public static int getRandomInt(int lower, int higher) {
         int interval = higher - lower;
         return (int) (Math.random() * (interval + 1)) + lower;
+    }
+
+
+    public static int[] randNums(int n, int max) {
+        ArrayList<Integer> samples = new ArrayList<>();
+        for(int i = 0; i < max; i++) samples.add(i); // 0 ~ max-1
+
+        int [] ret = new int[n];
+        int index;
+
+        for(int i = 0; i < n; i++) {
+            index = (int)Math.floor(Math.random()*(samples.size()));
+            index = index % samples.size();
+            ret[i] = samples.get(index);
+            samples.remove(index);
+        }
+
+        return ret;
     }
 }
